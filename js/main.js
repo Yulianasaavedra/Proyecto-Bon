6ceae8f6e0e2f2ac5524756a0f9905fe288a8460
Souser <sebastiancamach109@gmail.com>
2022-12-14 23:36:54 -0500

    Prueba de la api

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var BotonProducto1 = document.querySelector("#product > div > div:nth-child(1) > div > div > div > button");
+BotonProducto1.addEventListener("click", alerta);
+
+function alerta() {
+    alert("Prueba");
+}
